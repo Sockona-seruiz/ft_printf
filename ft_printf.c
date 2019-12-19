5370bdea07d0b1ec0d09ec4349035185da851dbe
Sébastien Ruiz <56959985+Sockona-seruiz@users.noreply.github.com>
2019-12-19 11:34:07 +0100

    Add files via upload

---
+++ ft_printf.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_printf.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: seruiz <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/02 11:06:38 by seruiz       #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/19 10:24:09 by seruiz      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		ft_launch_fct(char c, va_list *arg_list, t_list *t_struct)
+{
+	if (c == 's')
+		return (ft_print_str(va_arg(*arg_list, char *), t_struct));
+	else if (c == 'c')
+		return (ft_launch_char(arg_list, t_struct));
+	else if (c == 'd')
+		return (ft_itoa_base(va_arg(*arg_list, int), "0123456789", t_struct));
+	else if (c == 'u' || c == 'i')
+		return (ft_itoa_base(va_arg(*arg_list, long int),
+				"0123456789", t_struct));
+	else if (c == '%')
+		return (ft_launch_perc(t_struct));
+	else if (c == 'x')
+		return (ft_itoa_base(va_arg(*arg_list, long int),
+				"0123456789abcdef", t_struct));
+	else if (c == 'p')
+		return (ft_itoa_base_p(va_arg(*arg_list, unsigned long int),
+				"0123456789abcdef", t_struct));
+	else if (c == 'X')
+	{
+		return (ft_itoa_base(va_arg(*arg_list, long int),
+				"0123456789ABCDEF", t_struct));
+	}
+	return (-1);
+}
+
+int		ft_atoi(const char *str)
+{
+	int i;
+	int result;
+	int sign;
+
+	sign = 1;
+	result = 0;
+	i = 0;
+	while (48 <= str[i] && str[i] <= 57)
+	{
+		result = (str[i] - 48) + (result * 10);
+		i++;
+	}
+	return (result * sign);
+}
+
+int		ft_printf_2(t_list *t_struct, const char *str, va_list *arg_list)
+{
+	int i;
+	int res;
+
+	res = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (i != 0 && str[i - 1] == '%' && t_struct->spec != '%')
+		{
+			i = i + ft_fill_struct(t_struct, &str[i], arg_list);
+			if (ft_launch_fct(t_struct->spec, arg_list, t_struct) == -1)
+				return (-1);
+			if (t_struct->spec == '%' && str[i + 1] == '%')
+			{
+				t_struct->spec = (char)NULL;
+				i++;
+			}
+		}
+		else if (str[i] != '%')
+			t_struct->res += write(1, &str[i], 1);
+		i++;
+	}
+	return (t_struct->res);
+}
+
+int		ft_printf(const char *str, ...)
+{
+	va_list		arg_list;
+	t_list		*t_struct;
+	int			i;
+	int			res;
+
+	if ((t_struct = ft_calloc(sizeof(t_list), 1)) == 0)
+		return (-1);
+	t_struct->res = 0;
+	res = 0;
+	i = 0;
+	va_start(arg_list, str);
+	if ((res = ft_printf_2(t_struct, str, &arg_list)) == -1)
+	{
+		free(t_struct);
+		return (-1);
+	}
+	free(t_struct);
+	return (res);
+}
---
+++ ft_printf.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_printf.h                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: seruiz <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/03 15:38:16 by seruiz       #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/18 17:19:25 by seruiz      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct	s_list
+{
+	char 		flag;
+	int			width;
+	int			prec;
+	char		spec;
+	int			res;
+	int			isneg;
+}				t_list;
+int				ft_printf(const char *str,
+				...) __attribute__((format(printf,1,2)));
+size_t			ft_strlen(char *str);
+int				ft_launch_char(va_list *arg_list, t_list *t_struct);
+int				ft_launch_perc(t_list *t_struct);
+int				ft_atoi(const char *str);
+void			*ft_calloc(size_t count, size_t size);
+void			ft_clean_struct(t_list *t_struct);
+int				ft_fill_prec(t_list *t_struct, const char *str, va_list *arg_list, int i);
+int				ft_fill_struct(t_list *t_struct, const char *str, va_list *arg_list);
+int				ft_print_int(char *str, t_list *t_struct, int i);
+int				ft_print_str(char *str, t_list *t_struct);
+int				ft_itoa(long int n, t_list *t_struct);
+char			*calcul_base(unsigned long int number, char *base, int len, int count);
+void			ft_launch_fct_int(char *number, t_list *t_struct, int len);
+int				ft_itoa_base(long number, char *base, t_list *t_struct);
+int				ft_itoa_base_p(unsigned long int number, char *base, t_list *t_struct);
