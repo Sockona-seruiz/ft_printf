4327c07e89215d9424e9b8bd4b04f2dcd51f4160
Sébastien Ruiz <56959985+Sockona-seruiz@users.noreply.github.com>
2019-12-27 13:01:52 +0100

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,22 @@
 /*   By: seruiz <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/03 15:38:16 by seruiz       #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/27 12:56:54 by seruiz      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/27 13:01:19 by seruiz      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 typedef struct	s_list
 {
-	char 		flag;
+	char		flag;
 	int			width;
 	int			prec;
 	char		spec;
@@ -38,12 +41,17 @@ int				ft_launch_perc(t_list *t_struct);
 int				ft_atoi(const char *str);
 void			*ft_calloc(size_t count, size_t size);
 void			ft_clean_struct(t_list *t_struct);
-int				ft_fill_prec(t_list *t_struct, const char *s, va_list *arg_list, t_listint *lst);
-int				ft_fill_struct(t_list *t_struct, const char *str, va_list *arg_list);
+int				ft_fill_prec(t_list *t_struct, const char *s,
+				va_list *arg_list, t_listint *lst);
+int				ft_fill_struct(t_list *t_struct, const char *str,
+				va_list *arg_list);
 int				ft_print_int(char *str, t_list *t_struct, int i);
 int				ft_print_str(char *str, t_list *t_struct);
 int				ft_itoa(long int n, t_list *t_struct);
-char			*calcul_base(unsigned long int number, char *base, int len, int count);
+char			*calcul_base(unsigned long int number, char *base,
+				int len, int count);
 void			ft_launch_fct_int(char *number, t_list *t_struct, int len);
 int				ft_itoa_base(long number, char *base, t_list *t_struct);
-int				ft_itoa_base_p(unsigned long int number, char *base, t_list *t_struct);
+int				ft_itoa_base_p(unsigned long int number, char *base,
+				t_list *t_struct);
+#endif
